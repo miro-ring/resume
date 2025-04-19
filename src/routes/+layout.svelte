cc9db04b4cb016b325101ebbc405675535e0f8b9
miro-ring <miro.ring@kakaocorp.com>
2025-04-19 18:03:16 +0900

    feat: add mode switcher

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 	import AppSidebar from '$lib/components/app-sidebar.svelte';
+	import ModeSwitcher from '$lib/components/mode-switcher.svelte';
+	import { ModeWatcher } from 'mode-watcher';
 	import '../app.css';
 
 	let { children } = $props();
@@ -10,6 +12,8 @@
 	<AppSidebar />
 	<main class="ml-0 p-4 transition-all duration-300 data-[sidebar-open=true]:ml-64">
 		<Sidebar.Trigger class="mb-4" />
+		<ModeSwitcher />
+		<ModeWatcher />
 		{@render children?.()}
 	</main>
 </Sidebar.Provider>
