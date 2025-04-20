4b4b4a5e21e09d8bf7f16e9601352bf340b55320
miro-ring <miro.ring@kakaocorp.com>
2025-04-20 22:07:15 +0900

    Updates

---
+++ _app/immutable/nodes/2.DRLcGvKM.js
@@ -0,0 +1,1 @@
+import{t as a,a as o}from"../chunks/BlteUHj6.js";import"../chunks/D1nm5nN3.js";var e=a('<div class="space-y-4 pl-8 pt-4"><h1 class="text-3xl font-bold">내 소개</h1> <p>장동균입니다.</p> <p>FE 개발자 입니다.</p></div>');function i(p){var t=e();o(p,t)}export{i as component};
