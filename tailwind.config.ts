cf41c4cb23015f4eb0cbb344b2f8b3a75ac48a9e
miro-ring <miro.ring@kakaocorp.com>
2026-02-01 22:00:58 +0900

    feat: add thoughts at thirty

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -68,7 +68,7 @@ const config: Config = {
 				sm: 'calc(var(--radius) - 4px)'
 			},
 			fontFamily: {
-				sans: [...fontFamily.sans]
+				sans: ['"Pretendard Variable"', 'Pretendard', ...fontFamily.sans]
 			},
 			keyframes: {
 				'accordion-down': {
